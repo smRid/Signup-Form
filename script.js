ff4ddfc8f798395228a8b69081ba7bb8395d617a
smRid <omorsk911@gmail.com>
2025-03-24 17:55:33 +0600

    js update

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container")
+const signUpLink = document.querySelector(".signup-link")
+const signInLink = document.querySelector(".signin-link")
+
+
+signUpLink.addEventListener("click", ()=>{
+    container.classList.add("navigate");
+});
+
+signInLink.addEventListener("click", ()=>{
+    container.classList.remove("navigate");
+});
